e5cb41d115d3715257b06bd3f153764495542565
Roberto Ortega <62662524+rortegaru@users.noreply.github.com>
2020-12-16 16:53:09 +0000

    Update clean_data.py

--- agu_oss/clean_data.py
+++ agu_oss/clean_data.py
@@ -18,7 +18,7 @@ def open_and_clean(url):
     Pandas.DataFrame
 
     """
-
+#Create a Pandas data frame
     temp_df = pd.read_csv(url,
                           skiprows=69,
                           delim_whitespace=True)
